37be917210a3be46c99760428f1b38fe5efa02b3
Bagoes Ihsan Taufiqurrahman <52454754+TugasBagoes98@users.noreply.github.com>
2019-10-11 14:53:36 +0700

    Menghapus pengecekan status koneksi

--- E41181277_Bagoes_Ihsan_Taufiqurrahman/PHP-Pak-Nugroho/connector.php
+++ E41181277_Bagoes_Ihsan_Taufiqurrahman/PHP-Pak-Nugroho/connector.php
@@ -7,12 +7,4 @@
 
     $conn = mysqli_connect($host, $username, $password, $database);
 
-    if($conn == true)
-    {
-        echo "Successfully connected";
-    }else
-    {
-        echo "Nani dafuk";
-    }
-
 ?>
