963ef1281084e40f7ea53a3e9d65888254150406
Bagoes Ihsan Taufiqurrahman <52454754+TugasBagoes98@users.noreply.github.com>
2019-11-20 13:25:51 +0700

    Membuat conenction.php
    Menyambungkan database dengan website

---
+++ Project_Web_Revamp/assets/includes/connection.php
@@ -0,0 +1,10 @@
+<?php
+
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "db_rizquina";
+    $conn = mysqli_connect($host, $username, $password, $database);
+
+    //Check connection
+?>
