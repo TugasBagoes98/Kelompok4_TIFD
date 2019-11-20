1cb1abf51235735899db3921ad373f48582f4dba
Bagoes Ihsan Taufiqurrahman <52454754+TugasBagoes98@users.noreply.github.com>
2019-11-20 09:57:48 +0700

    Mengubah header.php, footer.php, index.html
    Memindah header dari index.html kedalam header.php dan footer dari index.html ke footer.php

---
+++ Project_Web_Revamp/assets/includes/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Rizquina Laptop </title>
+    <link rel="stylesheet" href="assets/style/bootstrap.css">
+    <link rel="stylesheet" href="assets/style/custom.css">
+</head>
+<body>
+    
+    <!-- Bismillah -->
+    <!-- Sinau Elene Dungane -->
+    
+    <!-- Nav Start -->
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a href="#" class="navbar-brand font-weight-bold"> Rizquina Laptop </a>
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle Nav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a href="#" class="nav-link"> Home </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link"> About </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link"> Blog </a>
+                    </li>
+                    <hr class="bg-light d-block d-md-none w-100">
+                    <li class="nav-item">
+                        <a href="#modalLogin" class="nav-link" role="button" data-toggle="modal"> Login </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link"> Register </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Nav End -->
