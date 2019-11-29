616c1e19683fca69bb3b9274e8ae88a1f888b5b2
Bagoes Ihsan Taufiqurrahman <52454754+TugasBagoes98@users.noreply.github.com>
2019-11-29 11:02:47 +0700

    Mengubah func_reset_pass.php, mengubah script_reset_pass.php
    Menyelesaikan function reset password untuk user

--- Project_Web_Revamp/assets/includes/func_reset_pass.php
+++ Project_Web_Revamp/assets/includes/func_reset_pass.php
@@ -5,10 +5,32 @@
     if(isset($_POST['resetPass']))
     {
         //Menyimpan data kedalam variabel
-        $token = $_POST['tokenUser'];
-        $passwordUser = $_POST['resetPassword'];
+        $token = htmlspecialchars(mysqli_real_escape_string($conn, $_POST['tokenUser']));
+        $passwordUser = htmlspecialchars(mysqli_real_escape_string($conn, $_POST['resetPassword']));
+        $hashedPassword = password_hash($passwordUser, PASSWORD_BCRYPT);
 
-        
+        //Query
+        $query = "update user set password_user = '".$hashedPassword."' where token_user = '".$token."'";
+        //Query Hapus Token
+        $query_token = "update user set token_user = '' where token_user = '".$token."'";
+
+        //Eksekusi query
+        if(mysqli_query($conn, $query))
+        {
+
+            if(mysqli_query($conn, $query_token))
+            {
+                header("Location: ../../index.php?resetpassword=success");
+            }else
+            {
+                header("Location: ../../index.php?resetpassword=fail");
+            }
+
+        }else
+        {
+            echo "Lah kok error";
+            print_r($result);
+        };
 
 
 
