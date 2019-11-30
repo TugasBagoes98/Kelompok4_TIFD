05d458aee893435d66d3d4888ac9fc4dc87a8393
Bagoes Ihsan Taufiqurrahman <52454754+TugasBagoes98@users.noreply.github.com>
2019-11-30 17:26:51 +0700

    Mengubah login_plg.php
    Menambahkan validasi apakah akun sudah diaktivasi atau belum ketika login

--- Project_Web_Revamp/assets/includes/login_plg.php
+++ Project_Web_Revamp/assets/includes/login_plg.php
@@ -25,31 +25,37 @@
 
                 if(password_verify($passwordPelanggan, $row['PASSWORD_USER']))
                 {
-                    //Memberikan status login berhasil
-                    $_SESSION['login'] = true;
-                    print_r($row);
-                    $_SESSION['ID_USER'] = $row['ID_USER'];
-                    $_SESSION['NAMA_USER'] = $row['NAMA_USER'];
-                    $_SESSION['EMAIL_USER'] = $row['EMAIL_USER'];
-                    $_SESSION['HAK_AKSES_USER'] = $row['HAK_AKSES_USER'];
-
-                    //Apabila ingat saya atau remember me dicentang
-                    if(isset($_POST['checkRememberMe']))
+                    if($row['STATUS_AKTIVASI'] == 0)
                     {
-                        //Menyimpan session login kedalam cookie
-                        setcookie('logEmailPelanggan', $row['EMAIL_USER'], time() + 60);
-                    }
+                        header("Location: ../../index.php?activestatus=false");
+                    }else
+                    {
+                        //Memberikan status login berhasil
+                        $_SESSION['login'] = true;
+                        print_r($row);
+                        $_SESSION['ID_USER'] = $row['ID_USER'];
+                        $_SESSION['NAMA_USER'] = $row['NAMA_USER'];
+                        $_SESSION['EMAIL_USER'] = $row['EMAIL_USER'];
+                        $_SESSION['HAK_AKSES_USER'] = $row['HAK_AKSES_USER'];
 
-                    header("Location: ../../index.php?login=success");
+                        //Apabila ingat saya atau remember me dicentang
+                        if(isset($_POST['checkRememberMe']))
+                        {
+                            //Menyimpan session login kedalam cookie
+                            setcookie('logEmailPelanggan', $row['EMAIL_USER'], time() + 60);
+                        }
+
+                        header("Location: ../../index.php?login=success");
+                    }
 
                 }else
                 {
-                    header("Location: ../../index.php?wrongpassword");
+                    header("Location: ../../index.php?wrongpassword=true");
                 }
 
             }else
             {
-                header("Location: ../../index.php?usernotfound");
+                header("Location: ../../index.php?usernotfound=true");
             }   
 
         }else
