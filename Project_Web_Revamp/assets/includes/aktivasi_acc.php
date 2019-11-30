60be29bb96011539e2fe449e250548336b55f61b
Bagoes Ihsan Taufiqurrahman <52454754+TugasBagoes98@users.noreply.github.com>
2019-11-30 15:48:54 +0700

    Mengubah aktivasi_acc.php
    Menambahkan proses aktivasi user

--- Project_Web_Revamp/assets/includes/aktivasi_acc.php
+++ Project_Web_Revamp/assets/includes/aktivasi_acc.php
@@ -2,6 +2,36 @@
 
     require_once "connection.php";
 
-    
+    if(isset($_GET['token']))
+    {
+
+        //Menyimpan data kedalam variabel
+        $tokenUser = $_GET['token'];
+        
+        //Query
+        $query_aktivasi = "update user set status_aktivasi = 1 where token_user = '".$tokenUser."'";
+        $query_hapus_token = "update user set token_user = '' where token_user = '".$tokenUser."'";
+
+        //Mengaktifkan akun
+        if(mysqli_query($conn, $query_aktivasi))
+        {
+            //Menghapus token aktivasi
+            if(mysqli_query($conn, $query_hapus_token))
+            {
+                header("Location: ../../success_activate.php?success=true");
+            }else
+            {
+                header("Location: ../../index.php?error=failedtoreset");
+            }
+        }else
+        {
+            header("Location: ../../index.php?error=failedtoactivate");
+        }
+
+
+    }else
+    {
+        header("Location: ../../index.php?error=accessdenied");
+    }
 
 ?>
