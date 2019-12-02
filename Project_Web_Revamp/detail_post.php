cc303a9db6a735363a44cc374a20b01bc8a2c6c7
Bagoes Ihsan Taufiqurrahman <52454754+TugasBagoes98@users.noreply.github.com>
2019-12-02 18:26:42 +0700

    Menambahkan detail_post.php
    Menambahkan halaman php baru untuk menampilka berita

---
+++ Project_Web_Revamp/detail_post.php
@@ -0,0 +1,36 @@
+<?php require_once "assets/includes/header.php";?>
+
+
+    <div class="container">
+        <!-- Row Start -->
+        <div class="row">
+            <!-- First Column Start -->
+            <div class="col-lg-8">
+                <!-- Title -->
+                <h1 class="mt-4"> Benarkah menjadi <em>trader professional</em> dapat menghasilkan 1000$ dalam 1 hari ? </h1>
+                <!-- Author -->
+                <p class="lead">
+                    By
+                    <a href="#" class="text-decoration-none"> Jutawan Binomo yang Sebenarnya </a>
+                </p>
+                <hr>
+                <p> Posted on 31 January, 2020 </p>
+                <hr>
+                <!-- Preview Image -->
+                <img src="assets/images/ebook-binomo.png" alt="preview_image_posting" class="img-fluid rounded">  
+            </div>
+            <!-- First Column End -->
+            <!-- Second Column Start -->
+            <!-- Second Column End --> 
+        </div>
+        <!-- Row End --> 
+    </div>
+
+
+<?php require_once "assets/includes/footer.php";?>
+<?php require_once "assets/includes/footer_modal.php";?>
+<?php require_once "assets/includes/footer_javascript.php";?>
+<script src="assets/javascript/script_login_plg.js"></script>
+<script src="assets/javascript/script_error_catch.js"></script>
+<script src="assets/javascript/script_blog.js"></script>
+<?php require_once "assets/includes/footer_close.php"?>
