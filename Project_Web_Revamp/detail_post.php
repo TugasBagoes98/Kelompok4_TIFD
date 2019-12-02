9676685b939cb05fd91cba30c70250e622d364c5
Bagoes Ihsan Taufiqurrahman <52454754+TugasBagoes98@users.noreply.github.com>
2019-12-02 19:18:49 +0700

    Mengubah detail_post.php
    Menyelesaikan tampilan detail posting

--- Project_Web_Revamp/detail_post.php
+++ Project_Web_Revamp/detail_post.php
@@ -17,10 +17,104 @@
                 <p> Posted on 31 January, 2020 </p>
                 <hr>
                 <!-- Preview Image -->
-                <img src="assets/images/ebook-binomo.png" alt="preview_image_posting" class="img-fluid rounded">  
+                <img src="assets/images/ebook-binomo.png" alt="preview_image_posting" class="img-fluid rounded my-2">
+                <!-- The News Start -->
+                <p class="lead">
+                    <em> "Jutaan orang tidak menyadari, bahwa mereka dapat menghasilkan 1000 dollar dalam 1 hari, tanpa keluar rumah." </em>
+                </p>
+                <p class="text-justify">
+                    Kalian pasti sudah tidak asing lagi dengan kata - kata tersebut, mengapa tidak ? setiap orang yang melihat <em>YouTube</em>
+                    pasti pernah mendengarkan kata - kata tersebut setidaknya 1 hari sekali tanpa menekan tombol skip ( eh admin keikutan).
+                    <br>
+                    <br>
+                    Ya kalian benar, kata - kata tersebut adalah ciri khas dari seorang <em> Budi Setiawan, Trader Professional </em> yang
+                    menjadi <em>ambassador</em> atau contoh dari pemanfaatan aplikasi bernama <em><strong>Binomo</strong></em>.
+                    Apa sih <em><strong> Binomo </strong></em> ini kok sampai - sampai menjadi viral di akhir tahun 2019 ? yuk kita telusuri.
+                </p>
+                <p class="text-justify">
+                    <em><strong>Binomo</strong></em> adalah salah satu web penyedia jasa trading, yang belakangan ini sedang viral karena jargon
+                    nya dapat menghasilkan 1000 USD dalam sehari tanpa keluar rumah. Namun siapa sangka, dibalik tawaran menggiurkan untuk menghasilkan
+                    1000 USD dalam sehari dipertanyakan legalitasnya.
+                    <br>
+                    <br>
+                    Situs website <em><strong>Binomo</strong></em> ( binomo.com dan binomo.net) ternyata
+                    terdaftar kedalam 58 domain website illegal di Indonesia menurut pernyataan dari Kepala Badan Pengawas Perdagangan Berjangka Komoditi Tjahya Widayanti.
+                </p>
+                <blockquote class="blockquote text-justify">
+                    Entitas-entitas tersebut biasanya menawarkan investasi berkedok forex dengan menjanjikan keuntungan tetap atau fixed income di luar kewajaran dalam bentuk 
+                    paket-paket investasi seperti Paket Silver, Gold, Platinum dan sebagainya untuk menarik calon korbannya.
+                    <footer class="blockquote-footer"> Tjahya Widayanti, Minggu (24/11/2019) </footer>
+                </blockquote>
+
+                <p class="text-justify">
+                    Nah, gimana nih menurut kalian ? apakah kita tetap bisa mendapatkan untung dari <em><strong>Binomo</strong></em>
+                    atau kita lebih baik menjauhinya dan tidak menggunakan nya ?
+                </p>
+                
+                <!-- The News End -->   
             </div>
             <!-- First Column End -->
             <!-- Second Column Start -->
+            <div class="col-lg-4">
+                 <!-- Search Widget -->
+                 <div class="card my-4">
+                    <h5 class="card-header"> Search </h5>
+                    <div class="card-body">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search for ...">
+                            <span class="input-group-append">
+                                <button class="btn btn-outline-secondary"> Go! </button>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <!-- Category Widget -->
+                <div class="card my-4">
+                    <h5 class="card-header"> Category </h5>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <ul class="list-unstyled">
+                                    <li>
+                                        <a href=""> Bisnis </a>
+                                    </li>
+                                    <li>
+                                        <a href=""> Komputer </a>
+                                    </li>
+                                    <li>
+                                        <a href=""> Gadget </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col-lg-6">
+                                <ul class="list-unstyled">
+                                    <li>
+                                        <a href=""> Laptop </a>
+                                    </li>
+                                    <li>
+                                        <a href=""> Keyboard </a>
+                                    </li>
+                                    <li>
+                                        <a href=""> Graphic Card</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Sidebar Widget -->
+                <div class="card my-4">
+                    <h5 class="card-header"> Sidebar Widget </h5>
+                    <div class="card-body">
+                        <div class="input-group">
+                            <input type="email" class="form-control" placeholder="Enter your e-mail ...">
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary"> Subscribe </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <!-- Second Column End --> 
         </div>
         <!-- Row End --> 
