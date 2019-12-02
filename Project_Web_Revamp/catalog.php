0474b9c2c1158dffe93ec572ae5709cccb777467
Bagoes Ihsan Taufiqurrahman <52454754+TugasBagoes98@users.noreply.github.com>
2019-12-02 18:03:05 +0700

    Mengubah catalog.php, Mengubah custom.ss, menambahkan 3 gambar untuk slider dalam katalog

--- Project_Web_Revamp/catalog.php
+++ Project_Web_Revamp/catalog.php
@@ -1,6 +1,245 @@
 <?php require_once "assets/includes/header.php"?>
 
 
+    <div class="container">
+        <div class="row">
+            <!-- Side Menu -->
+            <div class="col-lg-3">
+                <h1 class="my-4"> Catalog </h1>
+                <div class="list-group">
+                    <a href="#" class="list-group-item"> Asus </a>
+                    <a href="#" class="list-group-item"> Lenovo </a>
+                    <a href="#" class="list-group-item"> Acer </a>
+                </div>
+            </div>
+            <!-- Product Menu -->
+            <div class="col-lg-9">
+                <div class="carousel slide my-4" id="carouselHeader" data-ride="carousel">
+                    <div class="carousel-inner" role="listbox">
+                        <ol class="carousel-indicators">
+                            <li class="active" data-target="#carouselHeader" data-slide-to="0"></li>
+                            <li data-target="#carouselHeader" data-slide-to="1"></li>
+                            <li data-target="#carouselHeader" data-slide-to="2"></li>
+                        </ol>
+                        <div class="carousel-item active carousel-background-medium-one">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h3 class="text-shadow-black"> Servis Laptop harga terjangkau ?</h3>
+                                <h4 class="text-shadow-black"> Hanya disini, yuk daftar! </h4>
+                            </div>
+                        </div>
+                        <div class="carousel-item carousel-background-medium-two">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h3 class="text-shadow-black"> Bingung cari laptop dengan budget mu ? </h3>
+                                <h4 class="text-shadow-black"> Jangan khawatir kami solusinya! </h4>
+                            </div>
+                        </div>
+                        <div class="carousel-item carousel-background-medium-three">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h3 class="text-shadow-black"> Cari laptop bekas tapi berkualitas ? </h3>
+                                <h4 class="text-shadow-black"> Yuk daftar dan mampir ke toko kita! </h4>
+                            </div>
+                        </div>
+                        <a href="#carouselHeader" class="carousel-control-prev" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only"> Previous </span>
+                        </a>
+                        <a href="#carouselHeader" class="carousel-control-next" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only"> Next </span>
+                        </a>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-lg-4 col-md-6 col-sm-16 mb-4">
+                        <div class="card h-100">
+                            <a href="">
+                                <img src="assets/images/laptop_img.jpg" alt="card-img" class="card-img-top">
+                            </a>
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <a href=""> ASUS X455L </a>
+                                </h4>
+                                <h5> Rp. 5.999.999,00 </h5>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item"> Processor : Core i3 </li>
+                                <li class="list-group-item"> RAM : 4GB DDR3 </li>
+                                <li class="list-group-item"> VGA Card : NVidia GTX 1080 ti </li>
+                            </ul>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-lg-12 col-md-6 col-sm-12 my-2">
+                                        <a href="#" class="btn btn-block btn-primary" role="button"> Read More </a>
+                                    </div>
+                                    <div class="col-lg-12 col-md-6 col-sm-12">
+                                        <a href="#" class="btn btn-block btn-outline-secondary" role="button"> Add to cart </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-16 mb-4">
+                        <div class="card h-100">
+                            <a href="">
+                                <img src="assets/images/laptop_img.jpg" alt="card-img" class="card-img-top">
+                            </a>
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <a href=""> ASUS X455L </a>
+                                </h4>
+                                <h5> Rp. 5.999.999,00 </h5>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item"> Processor : Core i3 </li>
+                                <li class="list-group-item"> RAM : 4GB DDR3 </li>
+                                <li class="list-group-item"> VGA Card : NVidia GTX 1080 ti </li>
+                            </ul>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-lg-12 col-md-6 col-sm-12 my-2">
+                                        <a href="#" class="btn btn-block btn-primary" role="button"> Read More </a>
+                                    </div>
+                                    <div class="col-lg-12 col-md-6 col-sm-12">
+                                        <a href="#" class="btn btn-block btn-outline-secondary" role="button"> Add to cart </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-16 mb-4">
+                        <div class="card h-100">
+                            <a href="">
+                                <img src="assets/images/laptop_img.jpg" alt="card-img" class="card-img-top">
+                            </a>
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <a href=""> ASUS X455L </a>
+                                </h4>
+                                <h5> Rp. 5.999.999,00 </h5>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item"> Processor : Core i3 </li>
+                                <li class="list-group-item"> RAM : 4GB DDR3 </li>
+                                <li class="list-group-item"> VGA Card : NVidia GTX 1080 ti </li>
+                            </ul>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-lg-12 col-md-6 col-sm-12 my-2">
+                                        <a href="#" class="btn btn-block btn-primary" role="button"> Read More </a>
+                                    </div>
+                                    <div class="col-lg-12 col-md-6 col-sm-12">
+                                        <a href="#" class="btn btn-block btn-outline-secondary" role="button"> Add to cart </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-16 mb-4">
+                        <div class="card h-100">
+                            <a href="">
+                                <img src="assets/images/laptop_img.jpg" alt="card-img" class="card-img-top">
+                            </a>
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <a href=""> ASUS X455L </a>
+                                </h4>
+                                <h5> Rp. 5.999.999,00 </h5>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item"> Processor : Core i3 </li>
+                                <li class="list-group-item"> RAM : 4GB DDR3 </li>
+                                <li class="list-group-item"> VGA Card : NVidia GTX 1080 ti </li>
+                            </ul>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-lg-12 col-md-6 col-sm-12 my-2">
+                                        <a href="#" class="btn btn-block btn-primary" role="button"> Read More </a>
+                                    </div>
+                                    <div class="col-lg-12 col-md-6 col-sm-12">
+                                        <a href="#" class="btn btn-block btn-outline-secondary" role="button"> Add to cart </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-16 mb-4">
+                        <div class="card h-100">
+                            <a href="">
+                                <img src="assets/images/laptop_img.jpg" alt="card-img" class="card-img-top">
+                            </a>
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <a href=""> ASUS X455L </a>
+                                </h4>
+                                <h5> Rp. 5.999.999,00 </h5>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item"> Processor : Core i3 </li>
+                                <li class="list-group-item"> RAM : 4GB DDR3 </li>
+                                <li class="list-group-item"> VGA Card : NVidia GTX 1080 ti </li>
+                            </ul>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-lg-12 col-md-6 col-sm-12 my-2">
+                                        <a href="#" class="btn btn-block btn-primary" role="button"> Read More </a>
+                                    </div>
+                                    <div class="col-lg-12 col-md-6 col-sm-12">
+                                        <a href="#" class="btn btn-block btn-outline-secondary" role="button"> Add to cart </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-16 mb-4">
+                        <div class="card h-100">
+                            <a href="">
+                                <img src="assets/images/laptop_img.jpg" alt="card-img" class="card-img-top">
+                            </a>
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <a href=""> ASUS X455L </a>
+                                </h4>
+                                <h5> Rp. 5.999.999,00 </h5>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item"> Processor : Core i3 </li>
+                                <li class="list-group-item"> RAM : 4GB DDR3 </li>
+                                <li class="list-group-item"> VGA Card : NVidia GTX 1080 ti </li>
+                            </ul>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-lg-12 col-md-6 col-sm-12 my-2">
+                                        <a href="#" class="btn btn-block btn-primary" role="button"> Read More </a>
+                                    </div>
+                                    <div class="col-lg-12 col-md-6 col-sm-12">
+                                        <a href="#" class="btn btn-block btn-outline-secondary" role="button"> Add to cart </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <ul class="pagination my-2">
+                    <li class="page-item">
+                        <a href="#" class="page-link" aria-label="previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li>
+                    <li class="page-item active"><a href="#" class="page-link">1</a></li>
+                    <li class="page-item"><a href="#" class="page-link">2</a></li>
+                    <li class="page-item"><a href="#" class="page-link">3</a></li>
+                    <li class="page-item"><a href="#" class="page-link">4</a></li>
+                    <li class="page-item"><a href="#" class="page-link">5</a></li>
+                    <li class="page-item">
+                        <a href="#" class="page-link" aria-label="next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+                </ul>
+            </div> 
+        </div>
+    </div>
+
 <?php require_once "assets/includes/footer.php";?>
 <?php require_once "assets/includes/footer_modal.php";?>
 <?php require_once "assets/includes/footer_javascript.php";?>
