e872b5b12d7f4fab98634a79326b56ee3e5d9358
Bagoes Ihsan Taufiqurrahman <52454754+TugasBagoes98@users.noreply.github.com>
2019-12-04 14:19:15 +0700

    mengubah header_dashboard_plg.php dan dashboard_plg.php
    Menambahkan header untuk dashboard

---
+++ Project_Web_Revamp/assets/includes/header_dashboard_plg.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Rizquina Laptop</title>
+    <link rel="stylesheet" href="assets/style/bootstrap.css">
+    <link rel="stylesheet" href="assets/style/custom.css"></title>
+</head>
+<body>
+    
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a href="index.php" class="navbar-brand font-weight-bold"> Rizquina Laptop </a>
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapsePlg" aria-controls="navbarCollapsePlg" aria-expanded="false" aria-label="Nav Toggle">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapsePlg">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="" class="nav-link"> Profile </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link"> Keranjang </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link"> Histori </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link"> Sign Out </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
---
+++ Project_Web_Revamp/dashboard_plg.php
@@ -0,0 +1,9 @@
+<?php require_once "assets/includes/header_dashboard_plg.php"?>
+
+    
+
+<?php require_once "assets/includes/footer.php";?>
+<?php require_once "assets/includes/footer_modal.php";?>
+<?php require_once "assets/includes/footer_javascript.php";?>
+<script src="assets/javascript/script_error_catch.js"></script>
+<?php require_once "assets/includes/footer_close.php"?>
