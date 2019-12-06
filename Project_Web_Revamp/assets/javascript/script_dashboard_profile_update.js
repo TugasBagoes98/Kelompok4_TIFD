1787626492f1c03aa63c70045fcbcb8f6df8d566
Bagoes Ihsan Taufiqurrahman <52454754+TugasBagoes98@users.noreply.github.com>
2019-12-06 21:28:40 +0700

    Mengubah script_profile_update.js dan dashboard_user_update_profile.php
    Menambahkan validasi dan mengubah struktur dokumen

--- Project_Web_Revamp/assets/javascript/script_dashboard_profile_update.js
+++ Project_Web_Revamp/assets/javascript/script_dashboard_profile_update.js
@@ -14,6 +14,7 @@ function profileActive()
 /* Parameter Validator */
 
 var regexOnlyLetter = new RegExp("^[a-zA-Z\ ]+$");
+var regexOnlySpace = new RegExp("^[ ]+$");
 var regexOnlyNumber = new RegExp("^[0-9]+$");
 var regexNumberAndLetter = new RegExp("^[a-zA-Z0-9\ ]+$");
 var regexNumberLetterAndCharacter = new RegExp("^[a-zA-Z0-9\ .,]+$");
@@ -30,3 +31,120 @@ function isEmpty(value)
         return false;
     }
 }
+
+function isOnlySpace(value)
+{
+    if(regexOnlySpace.test(value))
+    {
+        return true;
+    }else
+    {
+        return false;
+    }
+}
+
+/* The Form */
+var formUpdate = document.forms.formProfileUpdateUser;
+var namaUser = formUpdate.namaUserProfile;
+var alamatUser = formUpdate.alamatUserProfile;
+var notelpUser = formUpdate.notelpUserProfile;
+var emailUser = formUpdate.emailUserProfile;
+
+var oldEmail = emailUser.value;
+
+namaUser.onblur = function()
+{
+    if(isEmpty(namaUser.value))
+    {
+        namaUser.classList.remove('is-valid');
+        namaUser.classList.add('is-invalid');
+        document.getElementById('invalidUpdateNama').innerText = "Nama pengguna tidak boleh kosong.";
+    }else if(!regexOnlyLetter.test(namaUser.value))
+    {
+        namaUser.classList.remove('is-valid');
+        namaUser.classList.add('is-invalid');
+        document.getElementById('invalidUpdateNama').innerText = "Nama pengguna hanya boleh berisi huruf.";
+    }else if(isOnlySpace(namaUser.value))
+    {
+        namaUser.classList.remove('is-valid');
+        namaUser.classList.add('is-invalid');
+        document.getElementById('invalidUpdateNama').innerText = "Nama pengguna tidak boleh hanya berisi spasi.";
+    }else
+    {
+        namaUser.classList.remove('is-invalid');
+        namaUser.classList.add('is-valid');
+    }    
+}
+
+alamatUser.onblur = function()
+{
+    if(isEmpty(alamatUser.value))
+    {
+        alamatUser.classList.remove('is-valid');
+        alamatUser.classList.add('is-invalid');
+        document.getElementById('invalidUpdateAlamat').innerText = "Alamat pengguna tidak boleh kosong.";
+    }else if(!regexNumberLetterAndCharacter.test(alamatUser.value))
+    {
+        alamatUser.classList.remove('is-valid');
+        alamatUser.classList.add('is-invalid');
+        document.getElementById('invalidUpdateAlamat').innerText = "Alamat pengguna hanya boleh berisi huruf, angka, dan tanda titik / koma.";
+    }else if(isOnlySpace(alamatUser.value))
+    {
+        alamatUser.classList.remove('is-valid');
+        alamatUser.classList.add('is-invalid');
+        document.getElementById('invalidUpdateAlamat').innerText = "Alamat pengguna tidak boleh hanya berisi spasi.";
+    }else
+    {
+        alamatUser.classList.remove('is-invalid');
+        alamatUser.classList.add('is-valid');
+    }
+}
+
+notelpUser.onblur = function()
+{
+    if(isEmpty(notelpUser.value))
+    {
+        notelpUser.classList.remove('is-valid');
+        notelpUser.classList.add('is-invalid');
+        document.getElementById('invalidUpdateNotelp').innerText = "Nomor Telepon pengguna tidak boleh kosong.";
+    }else if(regexOnlyNumber.test(notelpUser.value))
+    {
+        notelpUser.classList.remove('is-valid');
+        notelpUser.classList.add('is-invalid');
+        document.getElementById('invalidUpdateNotelp').innerText = "Nomor Telepon pengguna hanya boleh berisi angka.";
+    }else if(isOnlySpace(notelpUser.value))
+    {
+        notelpUser.classList.remove('is-valid');
+        notelpUser.classList.add('is-invalid');
+        document.getElementById('invalidUpdateNotelp').innerText = "Nomor Telepon pengguna tidak boleh hanya berisi spasi.";   
+    }else
+    {
+        notelpUser.classList.remove('is-invalid');
+        notelpUser.classList.add('is-valid');
+    }
+}
+
+emailUser.onblur = function()
+{
+    if(isEmpty(emailUser.value))
+    {
+        emailUser.classList.remove('is-valid');
+        emailUser.classList.add('is-invalid');
+        document.getElementById('invalidUpdateEmail').innerText = "Email pengguna tidak boleh kosong.";
+    }else if(regexEmail.test(emailUser.value))
+    {
+        emailUser.classList.remove('is-valid');
+        emailUser.classList.add('is-invalid');
+        document.getElementById('invalidUpdateEmail').innerText = "Email pengguna invalid atau tidak sesuai, harap periksa kembali.";
+    }else if(isOnlySpace(emailUser.value))
+    {
+        emailUser.classList.remove('is-valid');
+        emailUser.classList.add('is-invalid');
+        document.getElementById('invalidUpdateEmail').innerText = "Email pengguna tidak boleh hanya berisi spasi.";
+    }else
+    {
+        emailUser.classList.remove('is-invalid');
+        emailUser.classList.add('is-valid');
+    }
+}
+
--- Project_Web_Revamp/dashboard_plg_update_profile.php
+++ Project_Web_Revamp/dashboard_plg_update_profile.php
@@ -29,26 +29,34 @@
             </div>
             <div class="col-lg-8">
                 <h3 class="my-2"> Profile User </h3>
-                <form method="post" id="formProfileUserDashboard">
+                <form method="post" id="formProfileUpdateUser" action="assets/includes/update_profile_user.php">
                     <div class="form-group">
                         <label for="namaUserProfile" class="font-weight-bold"> Nama User  </label>
                         <input type="text" name="namaUserProfile" id="namaUserProfile" class="form-control" value="<?php echo $fetch['NAMA_USER'];?>" placeholder="Enter your name ...." >
+                        <div class="valid-feedback" id="validUpdateNama">Bagus!</div>
+                        <div class="invalid-feedback" id="invalidUpdateNama"></div>
                     </div>
                     <div class="form-group">
                         <label for="alamatUserProfile" class="font-weight-bold"> Alamat User </label>
                         <textarea name="alamatUserProfile" id="alamatUserProfile" class="form-control" ><?php echo $fetch['ALAMAT_USER'];?></textarea>
+                        <div class="valid-feedback" id="validUpdateAlamat">Bagus!</div>
+                        <div class="invalid-feedback" id="invalidUpdateAlamat"></div>
                     </div>
                     <div class="form-group">
                         <label for="notelpUserProfile" class="font-weight-bold"> No. Handphone </label>
                         <input type="text" name="notelpUserProfile" id="notelpUserProfile" class="form-control" value="<?php echo $fetch['NO_HP_USER'];?>" placeholder="Enter your phone number...." >
+                        <div class="valid-feedback" id="validUpdateNotelp">Bagus!</div>
+                        <div class="invalid-feedback" id="invalidUpdateNotelp"></div>
                     </div>
                     <div class="form-group">
                         <label for="emailUserProfile" class="font-weight-bold"> Email User </label>
                         <input type="text" name="emailUserProfile" id="emailUserProfile" class="form-control" value="<?php echo $fetch['EMAIL_USER'];?>" placeholder="Enter your email...." >
+                        <div class="valid-feedback" id="validUpdateEmail">Bagus!</div>
+                        <div class="invalid-feedback" id="invalidUpdateEmail"></div>
                     </div>
                     <div class="form-group">
                         <label for="tanggalDaftarUser" class="font-weight-bold"> Tanggal Terdaftar </label>
-                        <input type="text" name="tanggalDaftarUser" id="tanggalDaftarUser" class="form-control" value="<?php echo $fetch['TANGGAL_DAFTAR'];?>" placeholder="Enter your date here..." >
+                        <input type="text" name="tanggalDaftarUser" id="tanggalDaftarUser" class="form-control" value="<?php echo $fetch['TANGGAL_DAFTAR'];?>" placeholder="Enter your date here..." disabled>
                     </div>
                     <div class="row">
                         <div class="col-lg-auto col-sm-6">
@@ -67,5 +75,5 @@
 <?php require_once "assets/includes/footer_modal.php";?>
 <?php require_once "assets/includes/footer_javascript.php";?>
 <script src="assets/javascript/script_error_catch.js"></script>
-<script src="assets/javascript/script_dashboard_profile.js"></script>
+<script src="assets/javascript/script_dashboard_profile_update.js"></script>
 <?php require_once "assets/includes/footer_close.php"?>
