a587076209185b5075c574a496d9d526c3f4d499
Bagoes Ihsan Taufiqurrahman <52454754+TugasBagoes98@users.noreply.github.com>
2019-12-17 21:27:36 +0700

    Mengubah pengarahan alamat dari detail laptop dan menyelesaikan transaksi

--- Project_Web_Revamp/assets/includes/checkout_cart.php
+++ Project_Web_Revamp/assets/includes/checkout_cart.php
@@ -3,12 +3,74 @@
 
     session_start();
 
+    require_once "connection.php";
+
 
     if(isset($_GET['click']))
     {
         if(isset($_SESSION['daftar_laptop']))
         {
+            //Menyimpan jumlah laptop yang dibeli
+            $total_laptop = count($_SESSION['daftar_laptop']);
+
+            //Query
+            //Mengambil transaksi terakhir
+            $query_get_latest_transaksi = "select * from transaksi order by id_transaksi desc limit 1";
+
+            //Menjalankan query
+            $query_get_latest_do = mysqli_query($conn, $query_get_latest_transaksi);
+
             
+            if(mysqli_num_rows($query_get_latest_do) > 0)
+            {
+                while($row = mysqli_fetch_assoc($query_get_latest_do))
+                {
+                    $id_transaksi = $row['ID_TRANSAKSI'] + 1;
+                    //Membuat Query
+                    $query_insert_transaksi = "insert into transaksi values('".$id_transaksi."', '".$_SESSION['ID_USER']."','".date('Y-m-d')."',0)";
+                    //Menjalankan query
+                    $run_query_insert = mysqli_query($conn,$query_insert_transaksi);
+                    if($run_query_insert)
+                    {
+                        for($i = 0; $i < $total_laptop; $i++)
+                        {
+                            //Membuat query untuk insert laptop
+                            $query_insert_laptop = "insert into detail_transaksi values('','".$id_transaksi."','".$_SESSION['daftar_laptop'][$i]."','1')";
+                            
+                            $run_query = mysqli_query($conn,$query_insert_laptop);
+
+                        }
+                        header("Location: ../../history_buy.php?successbuy=true&idtransaksi=".$id_transaksi);
+                    }else
+                    {
+                        header("Location: ../../shop_cart.php?systemerror=true");
+                    }
+                }
+            }else
+            {
+                $id_transaksi = 1;
+                //Membuat Query
+                $query_insert_transaksi = "insert into transaksi values('".$id_transaksi."', '".$_SESSION['ID_USER']."','".date('Y-m-d')."',0)";
+                //Menjalankan query
+                $run_query_insert = mysqli_query($conn,$query_insert_transaksi);
+                if($run_query_insert)
+                {
+                    for($i = 0; $i < $total_laptop; $i++)
+                    {
+                        //Membuat query untuk insert laptop
+                        $query_insert_laptop = "insert into detail_transaksi values('','".$id_transaksi."','".$_SESSION['daftar_laptop'][$i]."','1')";
+                        
+                        $run_query = mysqli_query($conn,$query_insert_laptop);
+
+                    }
+                    header("Location: ../../history_buy.php?successbuy=true&idtransaksi=".$id_transaksi);
+                }else
+                {
+                    header("Location: ../../shop_cart.php?systemerror=true");
+                }
+
+            }
+
         }else
         {
             header("Location: ../../shop_cart.php?emptylaptop=true");
