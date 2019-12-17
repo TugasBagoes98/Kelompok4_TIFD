74b696a492abe04f195c2fc36ef10d6d0a3d6ee1
Bagoes Ihsan Taufiqurrahman <52454754+TugasBagoes98@users.noreply.github.com>
2019-12-17 21:33:14 +0700

    Membuat histori transaksi

--- Project_Web_Revamp/history_buy.php
+++ Project_Web_Revamp/history_buy.php
@@ -1,3 +1,24 @@
-<?php?>
+<?php
+    require_once "assets/includes/header_dashboard_plg.php";
 
+    require_once "assets/includes/connection.php";
 
+    //Format Rupiah
+    function rupiah($value)
+    {
+        $hasil = "Rp. ".number_format($value,2,',','.');
+        return $hasil;
+    }
+?>
+
+    <div class="container">
+        
+    </div>
+
+<?php require_once "assets/includes/footer.php";?>
+<?php require_once "assets/includes/footer_modal.php";?>
+<?php require_once "assets/includes/footer_javascript.php";?>
+<script src="assets/javascript/script_login_plg.js"></script>
+<script src="assets/javascript/script_error_catch.js"></script>
+<script src="assets/javascript/script_history_transaksi.js"></script>
+<?php require_once "assets/includes/footer_close.php"?>
